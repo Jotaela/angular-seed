d0e1189bad82a7247daf075e95032f56a6b2f175
Jotaela <joanlluisfabra@gmail.com>
2020-01-03 13:37:03 +0100

    WIP

--- app/serveis/alumnesAPI.factory.js
+++ app/serveis/alumnesAPI.factory.js
@@ -4,16 +4,16 @@ angular.module('serveis')
     .factory('alumnesAPI', function ($http) {
         return {
             get: function () {
-                return $http.get('http://localhost:57915/api/Alumnes1');
+                return $http.get('http://localhost:57915/api/Alumnes');
             },
             post: function (content) {
-                return $http.post('http://localhost:57915/api/Alumnes1', content, {headers: {'Content-Type': 'application/json'}});
+                return $http.post('http://localhost:57915/api/Alumnes', content, {headers: {'Content-Type': 'application/json'}});
             },
             delete: function (id) {
-                return $http.delete('http://localhost:57915/api/Alumnes1/' + id);
+                return $http.delete('http://localhost:57915/api/Alumnes/' + id);
             },
             update: function (id, content) {
-                return $http.put('http://localhost:57915/api/Alumnes1/' + id, content);
+                return $http.put('http://localhost:57915/api/Alumnes/' + id, content);
             }
         };
     });
